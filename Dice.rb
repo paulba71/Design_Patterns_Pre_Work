83a9641d6202bd0766ac2102d0e43c83119c1655
paulba71 <paulba71@gmail.com>
2016-11-17 08:20:46 +0000

    Initial commit

---
+++ Dice.rb
@@ -0,0 +1,19 @@
+class DieMode
+  FOUR=1
+  EIGHT=2
+end
+
+class Direction
+  NORTH=1
+  EAST=2
+  SOUTH=3
+  WEST=4
+  NORTHEAST=5
+  SOUTHEAST=6
+  SOUTHWEST=7
+  NORTHWEES=8
+end
+
+class Dice
+  
+end
